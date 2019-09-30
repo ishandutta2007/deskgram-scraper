394091458c546b8a2ac5e1b216582af59f1ec2da
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-10-01 02:32:57 +0530

    removing instagram

--- quickstart.py
+++ quickstart.py
@@ -3,21 +3,21 @@ from igramscraper.deskgram import Deskgram
 import pprint as pp
 import traceback
 
-instagram = Instagram()
-instagram.with_credentials('username', 'password')
-instagram.login()
-for id in range(1, 10):
-    try:
-        account = instagram.get_account_by_id(id)
-        print('Id: ', account.identifier)
-        print('Username: ', account.username)
-        print('Full name: ', account.full_name)
-        print('Biography: ', account.biography)
-        print('Number of followers: ', account.followed_by_count)
-        # print(account)
-        print('========')
-    except Exception as e:
-        traceback.print_exc()
+# instagram = Instagram()
+# instagram.with_credentials('username', 'password')
+# instagram.login()
+# for id in range(1, 10):
+#     try:
+#         account = instagram.get_account_by_id(id)
+#         print('Id: ', account.identifier)
+#         print('Username: ', account.username)
+#         print('Full name: ', account.full_name)
+#         print('Biography: ', account.biography)
+#         print('Number of followers: ', account.followed_by_count)
+#         # print(account)
+#         print('========')
+#     except Exception as e:
+#         traceback.print_exc()
 
 deskgram = Deskgram()
 for id in range(1, 10):
